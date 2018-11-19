8f6f9e4ea5882bc6d19223ab50fe8538a6a3c25a
SallyHabib <sally.hanen@student.guc.edu.eg>
2018-11-19 21:32:59 +0200

    smoke particle system, trees and grass

---
+++ projet2/Assets/scripts/cameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+    public Camera mainCamera;
+    public Camera TopCamera;
+    // Use this for initialization
+    void Start () {
+        mainCamera.enabled = true;
+        TopCamera.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            TopCamera.enabled = !TopCamera.enabled;
+        }
+
+    }
+}
